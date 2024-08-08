8b8826587a0a84d36af333edeb1803bb48337040
Maxime <maxime.b2494@gmail.com>
2024-08-08 15:23:47 +0200

    panier + form adresse livraison

---
+++ src/app/module/shared/components/cart-item/cart-item.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-item',
+  templateUrl: './cart-item.component.html',
+  styleUrl: './cart-item.component.scss'
+})
+export class CartItemComponent {
+
+  updateCartItem(num: number) {
+    console.log("num", num);
+  }
+
+  removeCartItem() {
+    console.log("remove cart item");
+  }
+
+}
